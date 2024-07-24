dd76cc42a17ce69766522d4291106a658d88f623
RafaGms <faelgomes2003@gmail.com>
2024-07-24 14:52:07 -0300

    ♻️refact: final project, refactoring everything

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,21 +2,20 @@ import { Router } from "express";
 import uploadsConfig from "../config/multer";
 import multer from "multer";
 import userController from "../controller/userController";
-import transationController from "../controller/transationController";
+import transactionController from "../controller/transactionController";
 
 export const router = Router();
 
 const upload = multer(uploadsConfig);
 
-
 router.post('/users', upload.single("images"), userController.registerUser);
 router.post('/signIn', userController.signInUser);
 router.post('/token', userController.tokenUser);
 
-router.post('/transation', transationController.registerTransaction);
-router.get('/transations/:id', transationController.getAllTransactionById);
-router.delete('/transations/:id', transationController.delete);
-router.put('/transations/:id', transationController.update);
+router.post('/transaction', transactionController.registerTransaction);
+router.get('/transactions/:id', transactionController.getAllTransactionById);
+router.delete('/transactions/:id', transactionController.delete);
+router.put('/transactions/:id', transactionController.update);
 
 
 
